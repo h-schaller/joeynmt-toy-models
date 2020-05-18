de89d92a774e17974a0013d19ef2215a447adbe0
h-schaller <woelfli97@gmail.com>
2020-05-18 11:27:09 +0200

    consider language while tokenising

--- scripts/preprocess_data.sh
+++ scripts/preprocess_data.sh
@@ -19,11 +19,11 @@ mkdir -p $preprocessed_data
 #################################################################
 
 python3 $scripts/subsample.py --infile $data/train.de-en.$src --seed 1 --lines $num_lines --outfile prep.train.de-en.$src
-sacremoses tokenize < prep.train.de-en.$src > train.tok.de-en.$src
+sacremoses tokenize -l $src < prep.train.de-en.$src > train.tok.de-en.$src
 rm $base/prep.train.de-en.$src
 mv $base/train.tok.de-en.$src $preprocessed_data
 
 python3 $scripts/subsample.py --infile $data/train.de-en.$trg --seed 1 --lines $num_lines --outfile prep.train.de-en.$trg
-sacremoses tokenize < prep.train.de-en.$trg > train.tok.de-en.$trg
+sacremoses tokenize -l $trg < prep.train.de-en.$trg > train.tok.de-en.$trg
 rm $base/prep.train.de-en.$trg
 mv $base/train.tok.de-en.$trg $preprocessed_data
