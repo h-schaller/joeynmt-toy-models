6417b475b5917f471b7f518757daf91a8d1d63be
h-schaller <woelfli97@gmail.com>
2020-05-18 11:43:15 +0200

    tokenise test and dev data

--- scripts/preprocess_data.sh
+++ scripts/preprocess_data.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-# This script subsamples 100'000 lines from source and target language files and tokenises the sampled lines.
+# This script subsamples 100'000 lines from source and target language training files and tokenises the sampled lines.
+# Tokenisation also of test and dev text files.
 
 scripts=`dirname "$0"`
 base=$scripts/..
@@ -27,3 +28,15 @@ python3 $scripts/subsample.py --infile $data/train.de-en.$trg --seed 1 --lines $
 sacremoses tokenize -l $trg < prep.train.de-en.$trg > train.tok.de-en.$trg
 rm $base/prep.train.de-en.$trg
 mv $base/train.tok.de-en.$trg $preprocessed_data
+
+sacremoses tokenize -l $src < $data/dev.de-en.$src > dev.tok.de-en.$src
+mv dev.tok.de-en.$src $preprocessed_data
+
+sacremoses tokenize -l $src < $data/test.de-en.$src > test.tok.de-en.$src
+mv test.tok.de-en.$src $preprocessed_data
+
+sacremoses tokenize -l $trg < $data/dev.de-en.$trg > dev.tok.de-en.$trg
+mv dev.tok.de-en.$trg $preprocessed_data
+
+sacremoses tokenize -l $trg < $data/test.de-en.$trg > test.tok.de-en.$trg
+mv test.tok.de-en.$trg $preprocessed_data
