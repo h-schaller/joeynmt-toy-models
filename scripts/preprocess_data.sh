920323d10a742af1d3b81e3a541433cb81c0a494
h-schaller <woelfli97@gmail.com>
2020-05-18 11:15:45 +0200

    subsampling and preprocessing

---
+++ scripts/preprocess_data.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# This script subsamples 100'000 lines from source and target language files and tokenises the sampled lines.
+
+scripts=`dirname "$0"`
+base=$scripts/..
+
+data=$base/data
+preprocessed_data=$base/preprocessed_data
+tools=$base/tools
+
+src=de
+trg=en
+
+num_lines=100000
+
+mkdir -p $preprocessed_data
+
+#################################################################
+
+python3 $scripts/subsample.py --infile $data/train.de-en.$src --seed 1 --lines $num_lines --outfile prep.train.de-en.$src
+sacremoses tokenize < prep.train.de-en.$src > train.tok.de-en.$src
+rm $base/prep.train.de-en.$src
+mv $base/train.tok.de-en.$src $preprocessed_data
+
+python3 $scripts/subsample.py --infile $data/train.de-en.$trg --seed 1 --lines $num_lines --outfile prep.train.de-en.$trg
+sacremoses tokenize < prep.train.de-en.$trg > train.tok.de-en.$trg
+rm $base/prep.train.de-en.$trg
+mv $base/train.tok.de-en.$trg $preprocessed_data
