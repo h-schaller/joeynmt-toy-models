0de48aa705c83cf11b5bd142fddb7f35bcd5c3b2
h-schaller <woelfli97@gmail.com>
2020-05-19 15:59:17 +0200

    uncomment

--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -8,9 +8,9 @@ trained_models=$base/trained_models
 configs=$base/configs/
 translations=$base/translations
 
-#mkdir -p $translations
-#mkdir -p $models
-#mkdir -p $trained_models
+mkdir -p $translations
+mkdir -p $models
+mkdir -p $trained_models
 
 num_threads=12
 device=0
@@ -22,18 +22,18 @@ SECONDS=0
 # train models:
 
 # word-level
-#CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/word-level_de-en.yaml
-#source $base/scripts/evaluate_word-level.sh word-level_de-en
-#cp -r $models/word-level_de-en $trained_models/
-#rm -r $models/word-level_de-en
+CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/word-level_de-en.yaml
+source $base/scripts/evaluate_word-level.sh word-level_de-en
+cp -r $models/word-level_de-en $trained_models/
+rm -r $models/word-level_de-en
 
 # BPE with vocabulary size 2'000
-#CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/bpe_de-en_2000.yaml
-#source $base/scripts/evaluate_bpe-level.sh bpe_de-en_2000 2000
-#cp -r $models/bpe_de-en_2000 $trained_models/
-#rm -r $models/bpe_de-en_2000
+CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/bpe_de-en_2000.yaml
+source $base/scripts/evaluate_bpe-level.sh bpe_de-en_2000 2000
+cp -r $models/bpe_de-en_2000 $trained_models/
+rm -r $models/bpe_de-en_2000
 
-# BPE with vocabulary size 2'000
+# BPE with vocabulary size 3'000
 CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/bpe_de-en_3000.yaml
 source $base/scripts/evaluate_bpe-level.sh bpe_de-en_3000 3000
 cp -r $models/bpe_de-en_3000 $trained_models/
