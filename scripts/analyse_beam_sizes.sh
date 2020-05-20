000976431faede3c43c1da99dc23c7caebee7b6a
h-schaller <woelfli97@gmail.com>
2020-05-20 10:00:21 +0200

    graph beam sizes

--- scripts/analyse_beam_sizes.sh
+++ scripts/analyse_beam_sizes.sh
@@ -16,31 +16,33 @@ SECONDS=0
 mkdir -p $translations_beam_sizes
 
 python3 scripts/change_beam_sizes.py --infile configs/bpe_de-en_3000.yaml --beamsize 2 --outfile configs/'bpe_de-en_3000_new_beam-size.yaml'
-source $base/scripts/test_beam_sizes.sh 2
+source $base/scripts/test_beam_sizes.sh 2 > beam_size_results.txt
 
 python3 scripts/change_beam_sizes.py --infile configs/bpe_de-en_3000.yaml --beamsize 4 --outfile configs/'bpe_de-en_3000_new_beam-size.yaml'
-source $base/scripts/test_beam_sizes.sh 4
+source $base/scripts/test_beam_sizes.sh 4 >> beam_size_results.txt
 
 python3 scripts/change_beam_sizes.py --infile configs/bpe_de-en_3000.yaml --beamsize 6 --outfile configs/'bpe_de-en_3000_new_beam-size.yaml'
-source $base/scripts/test_beam_sizes.sh 6
+source $base/scripts/test_beam_sizes.sh 6 >> beam_size_results.txt
 
 python3 scripts/change_beam_sizes.py --infile configs/bpe_de-en_3000.yaml --beamsize 8 --outfile configs/'bpe_de-en_3000_new_beam-size.yaml'
-source $base/scripts/test_beam_sizes.sh 8
+source $base/scripts/test_beam_sizes.sh 8 >> beam_size_results.txt
 
 python3 scripts/change_beam_sizes.py --infile configs/bpe_de-en_3000.yaml --beamsize 10 --outfile configs/'bpe_de-en_3000_new_beam-size.yaml'
-source $base/scripts/test_beam_sizes.sh 10
+source $base/scripts/test_beam_sizes.sh 10 >> beam_size_results.txt
 
 python3 scripts/change_beam_sizes.py --infile configs/bpe_de-en_3000.yaml --beamsize 12 --outfile configs/'bpe_de-en_3000_new_beam-size.yaml'
-source $base/scripts/test_beam_sizes.sh 12
+source $base/scripts/test_beam_sizes.sh 12 >> beam_size_results.txt
 
 python3 scripts/change_beam_sizes.py --infile configs/bpe_de-en_3000.yaml --beamsize 14 --outfile configs/'bpe_de-en_3000_new_beam-size.yaml'
-source $base/scripts/test_beam_sizes.sh 14
+source $base/scripts/test_beam_sizes.sh 14 >> beam_size_results.txt
 
 python3 scripts/change_beam_sizes.py --infile configs/bpe_de-en_3000.yaml --beamsize 16 --outfile configs/'bpe_de-en_3000_new_beam-size.yaml'
-source $base/scripts/test_beam_sizes.sh 16
+source $base/scripts/test_beam_sizes.sh 16 >> beam_size_results.txt
 
 python3 scripts/change_beam_sizes.py --infile configs/bpe_de-en_3000.yaml --beamsize 18 --outfile configs/'bpe_de-en_3000_new_beam-size.yaml'
-source $base/scripts/test_beam_sizes.sh 18
+source $base/scripts/test_beam_sizes.sh 18 >> beam_size_results.txt
 
 python3 scripts/change_beam_sizes.py --infile configs/bpe_de-en_3000.yaml --beamsize 20 --outfile configs/'bpe_de-en_3000_new_beam-size.yaml'
-source $base/scripts/test_beam_sizes.sh 20
+source $base/scripts/test_beam_sizes.sh 20 >> beam_size_results.txt
+
+python3 scripts/beam_graph.py --infile beam_size_results.txt
---
+++ scripts/beam_graph.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python3
+
+import argparse
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--infile', type=str, help='path of file to read BLEU scores from', required=True)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    with open(args.infile, 'r') as infile:
+        file = infile.read()
+        str_scores = re.findall(r'=\s(\d\d\.\d)', file)
+        bleu_scores = []
+        for score in str_scores:
+            score = float(score)
+            bleu_scores.append(score)
+
+    beam_sizes = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+    plt.bar(beam_sizes, bleu_scores)
+    plt.ylim(15.9, 16.4)
+    plt.yticks(np.arange(16.0, 16.5, 0.1))
+    plt.xticks(np.arange(0, 22, 2.0))
+    plt.savefig("beam_graph.png")
+
+
+if __name__ == main():
+    main()
+
